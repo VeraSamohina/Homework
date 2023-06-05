8c66e8ad0247ebbf89bb06586879990da4469921
Vera Samohina <samohinavera44@gmail.com>
2023-06-05 21:19:15 +0300

    add second func in str_func.py and docstring

--- str_func.py
+++ str_func.py
@@ -1,2 +1,7 @@
 def upper_func(text):
+    """Функция возвращает строку заглавными буквами"""
     return text.upper()
+
+def title_func(text):
+    """Функция возвращает тескт, в котром каждое слово с болшрй буквы"""
+    return text.title()
